b52ce3981919637303a830398b4d0b26bb3a6771
Kim Johnson <memjay3279@gmail.com>
2021-08-01 10:58:30 -0400

    Extract Test Config

--- flask_oauth/app_test.py
+++ flask_oauth/app_test.py
@@ -1,15 +1,4 @@
-from flask import json, jsonify
-from . import create_app
-import pytest 
-
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config['TESTING'] = True
-
-    with app.app_context():
-        with app.test_client() as client:
-            yield client
+from .test_config import client
 
 def test_health_check(client):
     response = client.get('/_health')
---
+++ flask_oauth/test_config.py
@@ -0,0 +1,11 @@
+from . import create_app
+import pytest 
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
